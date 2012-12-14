beaf3451e973a1696a8ea6858203d58230e7eeab
Christopher Wellons <mosquitopsu@gmail.com>
2012-12-14 15:52:54 -0500

    Add arguments to the database.

--- js-lookup-database.el
+++ js-lookup-database.el
@@ -12,75 +12,81 @@
 (require 'js-lookup)
 
 (js-lookup/root "https://developer.mozilla.org/en-US/docs/"
+  (js-lookup/root "JavaScript/Reference/"
 
-  ;; Global Objects
-  (js-lookup/root "JavaScript/Reference/Global_Objects/"
-    (js-lookup/category Array
-      concat constructor every filter forEach indexOf join lastIndexOf length
-      map pop push reduce reduceRight reverse shift slice some sort splice
-      toString unshift)
-    (js-lookup/category Boolean
-      constructor toString valueOf)
-    (js-lookup/category Date
-      constructor getDate getDay getFullYear getHours getMilliseconds
-      getMinutes getMonth getSeconds getTime getTimezoneOffset getUTCDate
-      getUTCDay getUTCFullYear getUTCHours getUTCMilliseconds getUTCMinutes
-      getUTCMonth getUTCSeconds getYear setDate setFullYear setHours
-      setMilliseconds setMinutes setMonth setSeconds setTime setUTCDate
-      setUTCFullYear setUTCHours setUTCMilliseconds setUTCMinutes setUTCMonth
-      setUTCSeconds setYear toDateString toGMTString toISOString toJSON
-      toLocaleDateString toLocaleString toLocaleTimeString toString
-      toTimeString toUTCString valueOf)
-    (js-lookup/category Function
-      apply arguments bind call caller constructor length name toString)
-    (js-lookup/category Number
-      constructor toExponential toFixed toLocaleString toPrecision toString
-      valueOf)
-    (js-lookup/category Object
-      __defineGetter__ __defineSetter__ __lookupGetter__ __lookupSetter__
-      constructor hasOwnProperty isPrototypeOf propertyIsEnumerable
-      toLocaleString toString valueOf)
-    (js-lookup/category RegExp
-      constructor exec global ignoreCase lastIndex multiline source test
-      toString)
-    (js-lookup/category String
-      anchor big blink bold charAt charCodeAt concat constructor fixed
-      fontcolor fontsize indexOf italics lastIndexOf length link localeCompare
-      match replace search slice small split strike sub substr substring sup
-      toLocaleLowerCase toLocaleUpperCase toLowerCase toString toUpperCase trim
-      trimLeft trimRight valueOf)
+    ;; Global Objects
+    (js-lookup/root "Global_Objects/"
+      (js-lookup/category Array
+        concat constructor every filter forEach indexOf join lastIndexOf length
+        map pop push reduce reduceRight reverse shift slice some sort splice
+        toString unshift)
+      (js-lookup/category Boolean
+        constructor toString valueOf)
+      (js-lookup/category Date
+        constructor getDate getDay getFullYear getHours getMilliseconds
+        getMinutes getMonth getSeconds getTime getTimezoneOffset getUTCDate
+        getUTCDay getUTCFullYear getUTCHours getUTCMilliseconds getUTCMinutes
+        getUTCMonth getUTCSeconds getYear setDate setFullYear setHours
+        setMilliseconds setMinutes setMonth setSeconds setTime setUTCDate
+        setUTCFullYear setUTCHours setUTCMilliseconds setUTCMinutes setUTCMonth
+        setUTCSeconds setYear toDateString toGMTString toISOString toJSON
+        toLocaleDateString toLocaleString toLocaleTimeString toString
+        toTimeString toUTCString valueOf)
+      (js-lookup/category Function
+        apply arguments bind call caller constructor length name toString)
+      (js-lookup/category Number
+        constructor toExponential toFixed toLocaleString toPrecision toString
+        valueOf)
+      (js-lookup/category Object
+        __defineGetter__ __defineSetter__ __lookupGetter__ __lookupSetter__
+        constructor hasOwnProperty isPrototypeOf propertyIsEnumerable
+        toLocaleString toString valueOf)
+      (js-lookup/category RegExp
+        constructor exec global ignoreCase lastIndex multiline source test
+        toString)
+      (js-lookup/category String
+        anchor big blink bold charAt charCodeAt concat constructor fixed
+        fontcolor fontsize indexOf italics lastIndexOf length link
+        localeCompare match replace search slice small split strike sub substr
+        substring sup toLocaleLowerCase toLocaleUpperCase toLowerCase toString
+        toUpperCase trim trimLeft trimRight valueOf)
 
-    ;; Error Objects
-    (js-lookup/category Error
-      constructor message name toString)
-    (js-lookup/category EvalError)
-    (js-lookup/category RangeError)
-    (js-lookup/category ReferenceError)
-    (js-lookup/category SyntaxError)
-    (js-lookup/category TypeError)
-    (js-lookup/category URIError)
+      ;; Error Objects
+      (js-lookup/category Error
+        constructor message name toString)
+      (js-lookup/category EvalError)
+      (js-lookup/category RangeError)
+      (js-lookup/category ReferenceError)
+      (js-lookup/category SyntaxError)
+      (js-lookup/category TypeError)
+      (js-lookup/category URIError)
 
-    ;; Other Objects
-    (js-lookup/category Infinity
-      NEGATIVE_INFINITY POSITIVE_INFINITY)
-    (js-lookup/category JSON
-      parse stringify)
-    (js-lookup/category Math
-      E LN10 LN2 LOG10E LOG2E PI SQRT1_2 SQRT2 abs acos asin atan atan2 ceil
-      cos exp floor log max min pow random round sin sqrt tan)
-    (js-lookup/category NaN)
-    (js-lookup/category undefined)
+      ;; Other Objects
+      (js-lookup/category Infinity
+        NEGATIVE_INFINITY POSITIVE_INFINITY)
+      (js-lookup/category JSON
+        parse stringify)
+      (js-lookup/category Math
+        E LN10 LN2 LOG10E LOG2E PI SQRT1_2 SQRT2 abs acos asin atan atan2 ceil
+        cos exp floor log max min pow random round sin sqrt tan)
+      (js-lookup/category NaN)
+      (js-lookup/category undefined)
 
-    ;; Non-contructor Functions
-    (js-lookup/category decodeURI)
-    (js-lookup/category decodeURIComponent)
-    (js-lookup/category encodeURI)
-    (js-lookup/category encodeURIComponent)
-    (js-lookup/category eval)
-    (js-lookup/category isFinite)
-    (js-lookup/category isNaN)
-    (js-lookup/category parseFloat)
-    (js-lookup/category parseInt))
+      ;; Non-contructor Functions
+      (js-lookup/category decodeURI)
+      (js-lookup/category decodeURIComponent)
+      (js-lookup/category encodeURI)
+      (js-lookup/category encodeURIComponent)
+      (js-lookup/category eval)
+      (js-lookup/category isFinite)
+      (js-lookup/category isNaN)
+      (js-lookup/category parseFloat)
+      (js-lookup/category parseInt))
+
+    ;; Functions and Scope
+    (js-lookup/root "Functions_and_function_scope/"
+      (js-lookup/category arguments
+        callee caller length)))
 
   ;; Typed Arrays -- this section on MDN is mostly empty
   (js-lookup/root "JavaScript_typed_arrays/"
